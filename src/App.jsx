78b1fec1d24ae6aebe27d658151ad3a29ce9a306
Dongjae Heo <lindsay.heo@gmail.com>
2023-07-08 16:31:58 +1000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import Questions from "./Questions";
+import data from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [questions, setquestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    if (activeId === id) {
+      setActiveId(null);
+    } else {
+      setActiveId(id);
+    }
+  };
+  return (
+    <main>
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
+      ;
+    </main>
+  );
 };
 export default App;
